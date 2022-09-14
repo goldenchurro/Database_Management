3a53e4d3891deff79559791f30a1fc6bbc369520
Alex Tello <73854603+goldenchurro@users.noreply.github.com>
2022-09-13 20:03:55 -0700

    Update main.py

--- CS457/PA1/main.py
+++ CS457/PA1/main.py
@@ -9,3 +9,9 @@ TableList = [None]
 def inputCleaner(wordToRemove): # Removes ; and command
   query = UserQuery.replace(";", "")
   return query.replace(wordToRemove, "")
+
+def databaseExistenceCheck(db): # Checks if database exists
+  if db in subprocess.run(['ls', '|', 'grep', db], capture_output=True, text=True).stdout:
+    return 1
+  else:
+    return 0
